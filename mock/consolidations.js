34077fb42474eccd3b33b5d195d7079df4cdfc6a
Victor Torres <victor.torres_taking@totalexpress.com.br>
2026-07-01 17:30:23 -0300

    adicionando mock

---
+++ mock/consolidations.js
@@ -0,0 +1,23 @@
+{
+    "sku": "38271",
+    "name": "Good Girl Carolina Herrera Eau de Parfum - Perfume Feminino 80ml",
+    "description": "Perfume Importado feminino oriental floral. Para a mulher que ama seu lado bom e celebra o lado mau.",
+    "slugName": "good-girl-carolina-herrera-eau-de-parfum-perfume-feminino",
+    "images": {
+        "thumbnail": "https://res.cloudinary.com/beleza-na-web/image/upload/w_100,f_auto,fl_progressive,q_auto:best/v1/imagens/product/38271/4cc5bd40-0036-4406-8f73-aec5e7b97e51-good-girl-carolina-herrera-eau-de-parfum-perfume-feminino.png",
+        "large": "https://res.cloudinary.com/beleza-na-web/image/upload/w_1500,f_auto,fl_progressive,q_auto:best/v1/imagens/product/38271/4cc5bd40-0036-4406-8f73-aec5e7b97e51-good-girl-carolina-herrera-eau-de-parfum-perfume-feminino.png"
+    },
+    "price": 639.69,
+    "inventory": {
+        "sku": "38271",
+        "quantity": 89,
+        "committed": 12,
+        "available": 77
+    },
+    "priceSpecification": {
+        "sku": "38271",
+        "price": 639.69,
+        "maxPrice": 700,
+        "discountPercentage": 9
+    }
+}
